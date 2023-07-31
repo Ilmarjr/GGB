86121daf54131dc49c3825712f39561ae1156958
shinyronald <shinyronald10@gmail.com>
2023-07-31 20:56:57 -0300

    Alteração na classe entity, para um melhor cadastro de data

--- src/main/java/br/edu/unijorge/GGB/entitys/News.java
+++ src/main/java/br/edu/unijorge/GGB/entitys/News.java
@@ -1,9 +1,12 @@
 package br.edu.unijorge.GGB.entitys;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -14,13 +17,24 @@ public class News {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", nullable = false)
     private Long id;
+
     @Column(name = "TITLE")
     private String title;
-    @Column(name = "DATE")
-    private String date;
+
+    @Column(name = "DATE", updatable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataCadastro;
+
     @Column(name = "IMAGE")
     private String image;
+
     @Column(name = "DESCRIPTION")
     private String description;
+
+    @PrePersist
+    public void prePersist(){
+        setDataCadastro(LocalDate.now());
+    }
+
 
 }
